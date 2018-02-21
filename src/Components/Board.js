daeefcbdfb34d10045fbf5904183c39c321b8e70
fraser1234 <fraseralexanderblack@gmail.com>
2018-02-21 13:38:27 +0000

    images inserted

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -17,7 +17,7 @@ class Board extends React.Component {
     if(calculateWinner(squares) || squares[i]){
       return;
     }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    squares[i] = this.state.xIsNext ? <img src= {require('./happy.png')} width='40%'/> : <img src= {require('./sad.png')} width='40%'/>;
     this.setState({
       squares: squares,
       xIsNext: !this.state.xIsNext,
@@ -64,6 +64,27 @@ class Board extends React.Component {
       </div>
     );
   }
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 export default Board;
--- src/Components/Square.js
+++ src/Components/Square.js
@@ -7,3 +7,5 @@ function Square(props){
     </button>
   );
 }
+
+export default Square;
